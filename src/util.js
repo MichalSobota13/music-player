d656cf5aaf6e453c0011407a125975b6d4667c1d
MichalSobota13 <michalsobota13@gmail.com>
2023-06-13 16:15:20 +0200

    first commit

---
+++ src/util.js
@@ -0,0 +1,168 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHop() {
+  return [
+    {
+      name: "Jazz Cabbage",
+      artist: "Ian Ewing, Strehlow",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: true,
+      color: ["#C64D45", "#FCF1DD"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9363",
+    },
+    {
+      name: "Lagoons",
+      artist: "Strehlow, Chris Mazuera",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C64D45", "#FCF1DD"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8266",
+    },
+    {
+      name: "Sails",
+      artist: "Strehlow, Aylior",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C64D45", "#FCF1DD"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9355",
+    },
+    {
+      name: "Central Coast",
+      artist: "Ian Ewing, Strehlow",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C64D45", "#FCF1DD"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9364",
+    },
+    {
+      name: "Tamarind",
+      artist: "Ian Ewing, Strehlow, TyLuv.",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C64D45", "#FCF1DD"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8607",
+    },
+    {
+      name: "Cocktail Hour",
+      artist: "Strehlow",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C64D45", "#FCF1DD"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9352",
+    },
+    {
+      name: "Crema Cafe",
+      artist: "Strehlow, Glimlip",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C64D45", "#FCF1DD"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9357",
+    },
+    {
+      name: "Ocean Alley",
+      artist: "Strehlow, Aylior",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/06/49f6e32ca521fbad46a1b281e3893cf6254bf11d-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#C64D45", "#FCF1DD"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9360",
+    },
+    {
+      name: "Growing Through The Cra...",
+      artist: "Philanthrope, mommy",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#7E3BD1", "#2C20AF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=35636",
+    },
+    {
+      name: "Night Owl",
+      artist: "brillion.",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#7E3BD1", "#2C20AF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=35641",
+    },
+    {
+      name: "End of Youth",
+      artist: "When Mountains Move",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#7E3BD1", "#2C20AF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=35657",
+    },
+    {
+      name: "Rainbow Roll",
+      artist: "Strehlow",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#7E3BD1", "#2C20AF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=35642",
+    },
+    {
+      name: "Roaming",
+      artist: "L'Indécis",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#7E3BD1", "#2C20AF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=35662",
+    },
+    {
+      name: "Meadows",
+      artist: "fantompower",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2022/05/5158b15bf8cb3b9d50588cfc31d36d28c816dc0c-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#7E3BD1", "#2C20AF"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=35668",
+    },
+    {
+      name: "It's Okay",
+      artist: "Yasper",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2023/05/5683fa8ae05221c0d06eeedd90fe00e653d27c48-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#FEF1A2", "#7E5D4A"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=55163",
+    },
+    {
+      name: "Balcony",
+      artist: "Shofel",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2023/05/a553b95f790409f69feabdd6279f1153a11f14b3-1024x1024.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#5E6961", "#B9B9B7"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=35668",
+    },
+  ];
+}
+
+export default chillHop;
